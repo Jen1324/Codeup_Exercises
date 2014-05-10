fef841a34f3a8fc6afb08b5860748b0e4b9d4167
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-10 13:15:36 -0500

    These are my do-while exercises

--- do_while.php
+++ do_while.php
@@ -7,4 +7,21 @@ do {
 	$test++; 
 } while ($test <= 100);
 
+echo PHP_EOL;
 
+
+$test = 100;
+do {
+	echo "$test\n";
+	$test -= 5;
+	
+} while ($test >= -10);
+
+echo PHP_EOL;
+
+$test = 2;
+do {
+	echo "$test\n";
+	$test *= $test;
+	
+} while ($test <= 1000000);
