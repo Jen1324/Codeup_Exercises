5befaf3c260eba7250ec28f3106e3b6392f53726
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-13 10:38:29 -0500

    Added a way to choose an increment

--- for.php
+++ for.php
@@ -6,9 +6,12 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Please place an ending number.\n");
 $end = trim(fgets(STDIN));
 
+fwrite(STDOUT, "Please choose an increment. Example: 1's, 3's, 5's.\n");
+$inc = trim(fgets(STDIN));
+
 echo "Here are the numbers between $start and $end.\n";
 
-for ($num = $start; $num <= $end; $num++) {
+for ($num = $start; $num <= $end; $num += $inc) {
 	echo "$num\n";
 }
 
