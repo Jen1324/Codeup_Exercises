21bf614b44c49dd924f91f2781ab5351b10eaac0
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-13 11:07:16 -0500

    If user puts 0 in the increment box, it updates by 1

--- for.php
+++ for.php
@@ -6,13 +6,23 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Please place an ending number.\n");
 $end = trim(fgets(STDIN));
 
+//-for ($num = $start; $num <= $end; $num++) {
+
 fwrite(STDOUT, "Please choose an increment. Example: 1's, 3's, 5's.\n");
 $inc = trim(fgets(STDIN));
 
 echo "Here are the numbers between $start and $end.\n";
 
+//[ condition ] ? [value if true] : [ value if false ];
+
+if (empty($inc) || !is_numeric($inc)) {
+	$inc = 1;
+
+}
+
 for ($num = $start; $num <= $end; $num += $inc) {
 	echo "$num\n";
+
 }
 
 
