7a9e261f73b3b488442b95d9af9d4412e0a10c78
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-14 13:41:23 -0500

    switch statement checking with an if statement exercise

--- switch.php
+++ switch.php
@@ -5,29 +5,53 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = date('1');
+$day_of_week = date('N');
+$day_of_week = 6;
 
 switch($day_of_week) {
-    case '1':
+    case 1:
     	echo 'Monday' . PHP_EOL;
     	break;
-    case '2':
+    case 2:
     	echo 'Tuesday' .  PHP_EOL;
     	break;
-    case  '3':
+    case  3:
     	echo 'Wednesday' . PHP_EOL;
     	break;
-    case '4':
+    case 4:
     	echo 'Thursday' . PHP_EOL;
     	break;
-    case '5':
+    case 5:
     	echo 'Friday' . PHP_EOL;
     	break;
-    case '6':
+    case 6:
     	echo 'Saturday' . PHP_EOL;
     	break;
-    case '7':
+    case 7:
     	echo 'Sunday' . PHP_EOL;
     	break;
   
+}
+
+
+if ($day_of_week == 1) {
+	echo 'Monday' . PHP_EOL;
+}
+elseif ($day_of_week == 2) {
+	echo 'Tuesday' . PHP_EOL;
+}
+elseif ($day_of_week == 3) {
+	echo 'Wednesday' . PHP_EOL;
+}
+elseif ($day_of_week == 4) {
+	echo 'Thursday' . PHP_EOL;
+}
+elseif ($day_of_week == 5) {
+	echo 'Friday' . PHP_EOL;
+}
+elseif ($day_of_week == 6) {
+	echo 'Saturday' . PHP_EOL;
+}
+elseif ($day_of_week == 7) {
+	echo 'Sunday' . PHP_EOL;
 }
