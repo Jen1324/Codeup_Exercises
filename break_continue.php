ddbd0cc5904af93dd8437232abfe88dbd03b77eb
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-14 11:12:11 -0500

    Made a loop that counts from 1 to 100, but stops after 10 using break.

--- break_continue.php
+++ break_continue.php
@@ -1,15 +1,15 @@
 <?php
 
-// for ($i = 0; $i <= 100; $i += 2) {
-//     echo $i . "\n";
-//         continue; //exit the (for) loop
-//     }
-// echo "DONE!\n";
+for ($i = 2; $i <= 100; $i += 2) {
+    echo $i . "\n";
+        continue; //exit the (for) loop
+    }
+echo "DONE!\n";
 
-for ($i = 0; $i <= 100; $i++) {
+for ($i = 1; $i <= 100; $i++) {
 	echo $i . "\n";
-    if ($i % 10 == 10) { //evenly divided by two
-        break; //exit the (for) loop
+    if ($i >= 10) { 
+        break; 
     	
 	}
 }
