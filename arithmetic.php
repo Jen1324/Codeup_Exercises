2f5198ce69266fa2923aee968156d0f2dec90c55
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-15 10:03:40 -0500

    modulus testing

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,23 @@
 <?php
 
-function adder($a, $b) {
-	echo $a + $b . PHP_EOL;
+function modulus($a, $b) {
+	echo $a / $b . PHP_EOL;
 }
 
-function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
-}
+// function subtract($a, $b) {
+//     echo $a - $b . PHP_EOL;
+// }
 
-function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
-}
+// function multiply($a, $b) {
+//     echo $a * $b . PHP_EOL;
+// }
 
-function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
-}
+// function divide($a, $b) {
+//     echo $a / $b . PHP_EOL;
+// }
 
-adder (5, 5);
-subtract (5, 5);
-multiply (5, 5);
-divide (5, 5); 
+modulus (89, 5);
+// subtract (5, 5);
+// multiply (5, 5);
+// divide (5, 5); 
 
