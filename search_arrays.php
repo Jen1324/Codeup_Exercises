91f4b0c7303d53d1550a428b8a1e4de4f61d8567
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-19 15:28:05 -0500

    searching through arrays

--- search_arrays.php
+++ search_arrays.php
@@ -1,18 +1,35 @@
 <?php
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
-
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-function getnames($names) {
-	if (in_array('Tina', $names) !== FALSE) {
-		echo "Name found!\n";
+function array_has_value($needle, $haystack) {
+	if (array_search($needle, $haystack) === false) {
+		return false;
 	}	else {
-			echo "Name not found. Please try agian.\n";
+		  return true;
 		}
 
 }
 	
-getnames($names);
+// var_dump(array_has_value('Tina', $names));
+
+
+function array_common_count($array1, $array2) {
+	$count = 0;
+
+	foreach ($array1 as $value) {
+		if (array_has_value($value, $array2)) {
+			$count++;
+		}
+	}	
+	return $count;
+}
+
+var_dump(array_common_count($names, $compare));
+
+
+
+
 
 
