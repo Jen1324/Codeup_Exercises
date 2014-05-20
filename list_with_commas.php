98de28837e9c91f855d8178872d7168a564da27f
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-20 13:40:57 -0500

    First part of list_with_commas exercise done

---
+++ list_with_commas.php
@@ -0,0 +1,23 @@
+<?php
+
+function humanized_list($array) {
+
+}
+
+
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, and Tony Stark';
+$physicists_array = explode(', ', $physicists_string);
+$famous_fake_physicists = $physicists_string;
+	echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+	
+
+
+
+
+
+
+
+
+
+
+?>
